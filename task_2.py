ac6245b0e29fc1bf095a7008c942d04c57da9f49
sjasmeet <106386685+sjasmeet@users.noreply.github.com>
2022-05-30 14:13:03 +0530

    Update task_2.py
    - i) pass the elements to delete as parameters in function call, instead of hardcoding inside the function
    
                    ii) try to include the logic to delete "inParams" as entirely &
    
                                                                    contents of "outParams" (example - deleting datedef)

--- task_2.py
+++ task_2.py
@@ -11,26 +11,31 @@ Please write the modified json to a new file.
 import json
 import copy
 
-def remove_jsonkeys(data):
+def remove_jsonkeys(data,keys_to_delete):
     out_data = copy.deepcopy(data)
-    inner_key_to_delete = 'appdate'
-    outer_key_to_delete = 'outParams'
 
     for outerkey in data:
-        if isinstance(data[outerkey], (list, dict)):
+        if isinstance(data[outerkey], (dict)):
             for innerkey in data[outerkey]:
-                if innerkey == inner_key_to_delete:
-                    del out_data[outerkey][innerkey]
-        if outerkey == outer_key_to_delete:
+                if innerkey in keys_to_delete:
+                    del out_data[outerkey]
+                    break
+        elif isinstance(data[outerkey], (list)):
+            for index,innerkey in enumerate(data[outerkey]):
+                if innerkey in keys_to_delete:
+                    del out_data[outerkey][index]
+        elif outerkey in keys_to_delete:
             del out_data[outerkey]
 
     return out_data
 
 json_file = 'test_payload.json'
 
+keys_to_delete = ['appdate','dateterm','dateeff','sessionId']
+
 with open(json_file) as f:
     data = json.load(f)
-    new_data = remove_jsonkeys(data)
+    new_data = remove_jsonkeys(data,keys_to_delete)
 
 with open('test_payload_out.json', 'w') as f:
     json.dump(new_data, f, indent=2)
